400662b6243aea4dcd8a7c422b124773741b921c
Z-KN <1197916817@qq.com>
2024-02-28 09:28:06 -0600

    Initial commit. Built a very simple wireless model.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CS525-Project
+
+## How to run the code
+```bash
+cd src
+python3 wireless_model.py
+```
---
+++ src/wireless-model.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class Node:
+    def __init__(self, x, y, z, id, energy, range):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.id = id
+        self.energy = energy
+        self.range = range
+
+    def calc_distance(self, node):
+        return np.sqrt((self.x - node.x)**2 + (self.y - node.y)**2 + (self.z - node.z)**2)
+
+    def is_in_range(self, node):
+        return self.calc_distance(node) <= self.range
+
+    def __str__(self):
+        return f"Node {self.id} ({self.x}, {self.y}, {self.z})"
+    
+    def __repr__(self):
+        return f"Node {self.id} ({self.x}, {self.y}, {self.z})"
+    
+    def __eq__(self, other):
+        return self.id == other.id
+    
+def signal_strength(node, distance):
+    # TODO: this is a simple model, we should use a more complex model
+    return 1 / (1 + distance**2)
+
+# create 10 nodes
+nodes = [Node(np.random.randint(0, 100), np.random.randint(0, 100), 0, i, 100, 10) for i in range(10)]
