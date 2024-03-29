fe9e2aecb76aac76e25c876143926a6b9d16f59d
Z-KN <1197916817@qq.com>
2024-03-28 23:57:28 -0500

    Add vm 5 and 6

--- update_repo.sh
+++ update_repo.sh
@@ -3,3 +3,5 @@ scp -r /home/username/CS525-Project kainingz@sp24-cs525-0201.cs.illinois.edu:/ho
 scp -r /home/username/CS525-Project kainingz@sp24-cs525-0202.cs.illinois.edu:/home/kainingz/
 scp -r /home/username/CS525-Project kainingz@sp24-cs525-0203.cs.illinois.edu:/home/kainingz/
 scp -r /home/username/CS525-Project kainingz@sp24-cs525-0204.cs.illinois.edu:/home/kainingz/
+scp -r /home/username/CS525-Project kainingz@sp24-cs525-0205.cs.illinois.edu:/home/kainingz/
+scp -r /home/username/CS525-Project kainingz@sp24-cs525-0206.cs.illinois.edu:/home/kainingz/
