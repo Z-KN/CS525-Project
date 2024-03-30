0cd52f4e1dc9e0d2fc4d495a659bce1b24621138
Z-KN <1197916817@qq.com>
2024-03-30 11:22:34 -0500

    Try to run 10 VMs

--- network_test.py
+++ network_test.py
@@ -16,9 +16,16 @@ comm_dict = {"1": {"ip": "172.22.151.96", "port": "5567"},
              "4": {"ip": "172.22.151.210", "port": "5564"},
              "5": {"ip": "172.22.151.97", "port": "5563"},
              "6": {"ip": "172.22.153.6", "port": "5562"},
+             "7": {"ip": "172.22.154.233", "port": "5562"},
+             "8": {"ip": "172.22.151.211", "port": "5562"},
+             "9": {"ip": "172.22.151.98", "port": "5562"},
+             "10": {"ip": "172.22.153.7", "port": "5562"},
              }
 
-print(node_name)
+log_file=open("log.txt", "w")
+# print current time to log file
+print(time.ctime(), file=log_file)
+print(node_name, file=log_file)
 
 # Start a few nodes
 self = RaftNode(comm_dict, node_name)
@@ -36,7 +43,8 @@ else:
 time.sleep(5)
 
 # Check and see what the most recent entry is
-print(self.check_committed_entry())
+print(self.check_committed_entry(), file=log_file)
+print(time.ctime(), file=log_file)
 
 # Stop all the nodes
 self.stop()
--- script.sh
+++ script.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # this has a problem: can't execute in parallel; adding "&" does not help
-ssh kainingz@sp24-cs525-0201.cs.illinois.edu -t "pip3 install --user future==0.18.2 pyzmq==19.0.2 && chmod -R go+rw . && cd CS525-Project && python3 network_test.py -n 1" > logs/log1.txt 
-ssh kainingz@sp24-cs525-0202.cs.illinois.edu -t "pip3 install --user future==0.18.2 pyzmq==19.0.2 && chmod -R go+rw . && cd CS525-Project && python3 network_test.py -n 2" > logs/log2.txt
-ssh kainingz@sp24-cs525-0203.cs.illinois.edu -t "pip3 install --user future==0.18.2 pyzmq==19.0.2 && chmod -R go+rw . && cd CS525-Project && python3 network_test.py -n 3" > logs/log3.txt
-ssh kainingz@sp24-cs525-0204.cs.illinois.edu -t "pip3 install --user future==0.18.2 pyzmq==19.0.2 && chmod -R go+rw . && cd CS525-Project && python3 network_test.py -n 4" > logs/log4.txt
+ssh kainingz@sp24-cs525-0201.cs.illinois.edu -t "cd CS525-Project && python3 network_test.py -n 1 &" 
+ssh kainingz@sp24-cs525-0202.cs.illinois.edu -t "cd CS525-Project && python3 network_test.py -n 2 &" 
+ssh kainingz@sp24-cs525-0203.cs.illinois.edu -t "cd CS525-Project && python3 network_test.py -n 3 &" 
+ssh kainingz@sp24-cs525-0204.cs.illinois.edu -t "cd CS525-Project && python3 network_test.py -n 4 &"
+# ssh kainingz@sp24-cs525-0201.cs.illinois.edu -t "pip3 install --user future==0.18.2 pyzmq==19.0.2 && chmod -R go+rw . && cd CS525-Project && python3 network_test.py -n 5" > log5.txt 
+# ssh kainingz@sp24-cs525-0202.cs.illinois.edu -t "pip3 install --user future==0.18.2 pyzmq==19.0.2 && chmod -R go+rw . && cd CS525-Project && python3 network_test.py -n 6" > log6.txt
+# ssh kainingz@sp24-cs525-0203.cs.illinois.edu -t "pip3 install --user future==0.18.2 pyzmq==19.0.2 && chmod -R go+rw . && cd CS525-Project && python3 network_test.py -n 7" > log7.txt
+# ssh kainingz@sp24-cs525-0204.cs.illinois.edu -t "pip3 install --user future==0.18.2 pyzmq==19.0.2 && chmod -R go+rw . && cd CS525-Project && python3 network_test.py -n 8" > log8.txt
+# ssh kainingz@sp24-cs525-0204.cs.illinois.edu -t "pip3 install --user future==0.18.2 pyzmq==19.0.2 && chmod -R go+rw . && cd CS525-Project && python3 network_test.py -n 9" > log9.txt
+# ssh kainingz@sp24-cs525-0204.cs.illinois.edu -t "pip3 install --user future==0.18.2 pyzmq==19.0.2 && chmod -R go+rw . && cd CS525-Project && python3 network_test.py -n 10" > log10.txt
