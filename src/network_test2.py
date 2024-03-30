2c2e04ea08086dda0d128debfaa6ddff96c2c461
Z-KN <1197916817@qq.com>
2024-03-30 18:23:43 -0500

    Run network_test2 successfully

--- src/network_test2.py
+++ src/network_test2.py
@@ -19,10 +19,10 @@ def get_system_time():
         return time.perf_counter_ns()
 
 # Create the intercommunication json 
-comm_dict = {"01": {"ip": "172.22.151.96", "port": "5567"}, 
-             "02": {"ip": "172.22.153.5", "port": "5566"},
-             "03": {"ip": "172.22.154.232", "port": "5565"},
-             "04": {"ip": "172.22.151.210", "port": "5564"}}#,
+comm_dict = {"1": {"ip": "172.22.151.96", "port": "5567"}, 
+             "2": {"ip": "172.22.153.5", "port": "5566"},
+             "3": {"ip": "172.22.154.232", "port": "5565"},
+             "4": {"ip": "172.22.151.210", "port": "5564"}}#,
              #"5": {"ip": "172.22.151.97", "port": "5563"},
              #"6": {"ip": "172.22.153.6", "port": "5562"},
              #"7": {"ip": "172.22.154.233", "port": "5562"},
@@ -31,7 +31,7 @@ comm_dict = {"01": {"ip": "172.22.151.96", "port": "5567"},
              #"10": {"ip": "172.22.153.7", "port": "5562"},
              #}
 
-log_file=open("log.txt", "a")
+log_file=open("log.txt", "w")
 # print current time to log file
 print(get_system_time(), file=log_file)
 print(node_name, file=log_file)
@@ -49,14 +49,14 @@ self.client_request({'val': node_name})
 #time.sleep(2)
 last_entry = self.check_committed_entry
 print(self.check_committed_entry(), file=log_file)
-print(get_system_time(), file=log_file)
+print(get_system_time()/1e9, file=log_file)
 # Check and see what the most recent entry is
 while(1):
     try:
         if self.check_committed_entry() is not last_entry:
             last_entry = self.check_committed_entry
             print(self.check_committed_entry(), file=log_file)
-            print(get_system_time(), file=log_file)
+            print(get_system_time()/1e9, file=log_file)
         time.sleep(0.01)
     except KeyboardInterrupt:
         self.stop()
