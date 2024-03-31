670baa7160bde9b1c8812231bb8170e29ca91f61
Z-KN <1197916817@qq.com>
2024-03-30 19:20:28 -0500

    Add pause and unpause

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # this has a problem: can't execute in parallel; adding "&" does not help
-ssh kainingz@sp24-cs525-0201.cs.illinois.edu -t "cd CS525-Project && python3 src/network_test2.py -n 1" 
-ssh kainingz@sp24-cs525-0202.cs.illinois.edu -t "cd CS525-Project && python3 src/network_test2.py -n 2" 
-ssh kainingz@sp24-cs525-0203.cs.illinois.edu -t "cd CS525-Project && python3 src/network_test2.py -n 3" 
-ssh kainingz@sp24-cs525-0204.cs.illinois.edu -t "cd CS525-Project && python3 src/network_test2.py -n 4"
+ssh kainingz@sp24-cs525-0201.cs.illinois.edu -t "cd CS525-Project && python3 src/network_test_churn.py -n 1" 
+ssh kainingz@sp24-cs525-0202.cs.illinois.edu -t "cd CS525-Project && python3 src/network_test_churn.py -n 2" 
+ssh kainingz@sp24-cs525-0203.cs.illinois.edu -t "cd CS525-Project && python3 src/network_test_churn.py -n 3" 
+ssh kainingz@sp24-cs525-0204.cs.illinois.edu -t "cd CS525-Project && python3 src/network_test_churn.py -n 4"
 # ssh kainingz@sp24-cs525-0201.cs.illinois.edu -t "pip3 install --user future==0.18.2 pyzmq==19.0.2 && chmod -R go+rw . && cd CS525-Project && python3 network_test.py -n 5" > log5.txt 
 # ssh kainingz@sp24-cs525-0202.cs.illinois.edu -t "pip3 install --user future==0.18.2 pyzmq==19.0.2 && chmod -R go+rw . && cd CS525-Project && python3 network_test.py -n 6" > log6.txt
 # ssh kainingz@sp24-cs525-0203.cs.illinois.edu -t "pip3 install --user future==0.18.2 pyzmq==19.0.2 && chmod -R go+rw . && cd CS525-Project && python3 network_test.py -n 7" > log7.txt
